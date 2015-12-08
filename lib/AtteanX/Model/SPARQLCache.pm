8df4f24e580418dfe86e97dc85c90dd9dd5689c6
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2015-12-08 22:53:00 +0100

    rudimentary cost planning

--- lib/AtteanX/Model/SPARQLCache.pm
+++ lib/AtteanX/Model/SPARQLCache.pm
@@ -22,4 +22,17 @@ sub plans_for_algebra {
 	return;
 }
 
+sub cost_for_plan { # TODO: Do this for real
+	my $self	= shift;
+	my $plan	= shift;
+	if ($plan->isa('Attean::Plan::Quad')) {
+		return 3;
+	} elsif ($plan->isa('Attean::Plan::Table')) {
+		return 2;
+	} elsif ($plan->isa('AtteanX::Store::SPARQL::Plan::BGP')) {
+		return 20;
+	}
+	return;
+}
+
 1;
