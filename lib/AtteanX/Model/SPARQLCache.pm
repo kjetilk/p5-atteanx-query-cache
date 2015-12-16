41c813c7725af4cf07405a8aa1e6db3d202be384
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2015-12-16 22:09:20 +0100

    allow join types in cost too

--- lib/AtteanX/Model/SPARQLCache.pm
+++ lib/AtteanX/Model/SPARQLCache.pm
@@ -26,13 +26,13 @@ sub cost_for_plan { # TODO: Do this for real
  	my $self	= shift;
  	my $plan	= shift;
  	if ($plan->does('Attean::API::Plan::Join') && ${$plan->children}[0]->isa('Attean::Plan::Quad') && ${$plan->children}[1]->isa('Attean::Plan::Quad')) {
- 		return 10000;
- 	} elsif ($plan->isa('Attean::Plan::Table')) {
+		if ($plan->isa('Attean::Plan::HashJoin')) {
+			return 9900;
+		} elsif ($plan->isa('Attean::Plan::NestedLoopJoin')) {
+	 		return 10000;
+		}
+	} elsif ($plan->isa('Attean::Plan::Table')) {
  		return 2;
-# 	} elsif ($plan->isa('Attean::Plan::HashJoin')) {
-# 		return 2;
-# 	} elsif ($plan->isa('Attean::Plan::NestedLoopJoin')) {
-# 		return 3;
  	}
  	return;
 }
