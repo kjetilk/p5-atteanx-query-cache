2ba0b59e9c18c23d6491faada8c5ba1bf501c6d6
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2015-12-16 02:22:44 +0100

    Provide costs for some plans

--- lib/AtteanX/Model/SPARQLCache.pm
+++ lib/AtteanX/Model/SPARQLCache.pm
@@ -22,19 +22,19 @@ sub plans_for_algebra {
 	return;
 }
 
-# sub cost_for_plan { # TODO: Do this for real
-# 	my $self	= shift;
-# 	my $plan	= shift;
-# 	if ($plan->isa('Attean::Plan::Quad')) {
-# 		return 3;
-# 	} elsif ($plan->isa('Attean::Plan::Table')) {
-# 		return 2;
+sub cost_for_plan { # TODO: Do this for real
+ 	my $self	= shift;
+ 	my $plan	= shift;
+ 	if ($plan->does('Attean::API::Plan::Join') && ${$plan->children}[0]->isa('Attean::Plan::Quad') && ${$plan->children}[1]->isa('Attean::Plan::Quad')) {
+ 		return 1;
+ 	} elsif ($plan->isa('Attean::Plan::Table')) {
+ 		return 2;
 # 	} elsif ($plan->isa('Attean::Plan::HashJoin')) {
 # 		return 2;
 # 	} elsif ($plan->isa('Attean::Plan::NestedLoopJoin')) {
 # 		return 3;
-# 	}
-# 	return;
-# }
+ 	}
+ 	return;
+}
 
 1;
