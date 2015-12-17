d788f4563c21511b15113cdbba0708612219d94b
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2015-12-17 13:40:38 +0100

    Lower the cost of Join(Table,SPARQLBGP) by dividing with a factor

--- lib/AtteanX/Model/SPARQLCache.pm
+++ lib/AtteanX/Model/SPARQLCache.pm
@@ -25,11 +25,14 @@ sub plans_for_algebra {
 sub cost_for_plan { # TODO: Do this for real
  	my $self	= shift;
  	my $plan	= shift;
- 	if ($plan->does('Attean::API::Plan::Join') && ${$plan->children}[0]->isa('Attean::Plan::Quad') && ${$plan->children}[1]->isa('Attean::Plan::Quad')) {
-		if ($plan->isa('Attean::Plan::HashJoin')) {
-			return 9900;
-		} elsif ($plan->isa('Attean::Plan::NestedLoopJoin')) {
-	 		return 10000;
+	my $joinfactor = ($plan->isa('Attean::Plan::HashJoin')) ? 9 : 10; # Consistently prefer HashJoins unless treated specially
+	if ($plan->does('Attean::API::Plan::Join')) {
+		if (${$plan->children}[0]->isa('Attean::Plan::Quad') && ${$plan->children}[1]->isa('Attean::Plan::Quad')) {
+			return $joinfactor * 1000
+		} elsif (${$plan->children}[0]->isa('Attean::Plan::Table') && ${$plan->children}[1]->isa('AtteanX::Store::SPARQL::Plan::BGP')) {
+			return int(@{$plan->children}[1]->cost * $joinfactor / 20);
+		} elsif (${$plan->children}[1]->isa('Attean::Plan::Table') && ${$plan->children}[0]->isa('AtteanX::Store::SPARQL::Plan::BGP')) {
+			return int(@{$plan->children}[0]->cost * $joinfactor / 15);
 		}
 	} elsif ($plan->isa('Attean::Plan::Table')) {
  		return 2;
