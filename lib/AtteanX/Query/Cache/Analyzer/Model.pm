fd53400d019e0e66e38073a4a9d3764784e3b601
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2015-12-26 00:35:48 +0100

    Add a Model to store the things the analyzer will compute plans for

---
+++ lib/AtteanX/Query/Cache/Analyzer/Model.pm
@@ -0,0 +1,16 @@
+package AtteanX::Query::Cache::Analyzer::Model;
+
+use v5.14;
+use warnings;
+
+use Moo;
+use Types::Standard qw(ArrayRef Str);
+use namespace::clean;
+
+extends 'AtteanX::Model::SPARQLCache';
+
+has 'cached' => (is = 'ro', isa => ArrayRef[Str], required => 1);
+
+has 'try' => (is 'rw', isa => Str);
+
+1;
