2c957d711e7783b079a373ba355634ada31e67e5
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2015-12-30 00:39:07 +0100

    some very crude code in the analyzer

--- lib/AtteanX/Query/Cache/Analyzer.pm
+++ lib/AtteanX/Query/Cache/Analyzer.pm
@@ -9,10 +9,12 @@ our $VERSION   = '0.001';
 
 use Moo;
 use Attean::RDF qw(triplepattern variable iri);
-use Types::Standard qw(Str InstanceOf);
+use Types::Standard qw(Str Int InstanceOf);
 use Types::URI -all;
 use AtteanX::Parser::SPARQL;
 use AtteanX::Query::Cache::Analyzer::Model;
+use AtteanX::QueryPlanner::Cache;
+use AtteanX::Query::Cache::Analyzer::QueryPlanner;
 
 use Carp;
 
@@ -21,22 +23,31 @@ has 'base_uri' => (is => 'ro', default => 'http://default.invalid/');
 
 has 'model' => (is => 'ro', isa => InstanceOf['AtteanX::Query::Cache::Analyzer::Model'], required => 1);
 
-has 'graph' => (is => 'ro', isa => InstanceOf['Attean::IRI'], default => iri('http://example.invalid'));
+has 'graph' => (is => 'ro', isa => InstanceOf['Attean::IRI'], default => sub { return iri('http://example.invalid')});
+
+has 'threshold' => (is => 'ro', isa => Int, default => '10');
 
 sub analyze {
 	my $self = shift;
 	my $parser = AtteanX::Parser::SPARQL->new();
 	my ($algebra) = $parser->parse_list_from_bytes($self->query, $self->base_uri); # TODO: this is a bit of cargocult
+	# First, we find the cost of the plan with the current cache:
+	my $curplanner = AtteanX::QueryPlanner::Cache->new;
+	my $curplan = $curplanner->plan_for_algebra($algebra, $self->model, [$self->graph]);
+	my $curcost = $curplanner->cost_for_plan($curplan, $self->model);
+	warn $curcost;
 	my %costs;
-	my $planner = AtteanX::Query::Cache::Analyzer::QueryPlanner->new;
+	my $planner = $curplanner; #AtteanX::Query::Cache::Analyzer::QueryPlanner->new;
 	foreach my $bgp ($algebra->subpatterns_of_type('Attean::Algebra::BGP')) {
 		foreach my $triple (@{ $bgp->triples }) { # TODO: May need quads
-			next if ($model->is_cached($triple));
+			next if ($self->model->is_cached($triple));
 			my $key = $triple->canonicalize->as_string;
-			my $plan = $planner->plan_for_algebra($algebra, $model, [$self->graph]);
-			$costs{$key} = $planner->cost_for_plan($plan);
+			$self->model->try($key);
+			my $plan = $planner->plan_for_algebra($algebra, $self->model, [$self->graph]);
+			$costs{$key} = $planner->cost_for_plan($plan, $self->model);
 		}
 	}
+	warn Data::Dumper::Dumper(\%costs);
 }
 
 1;
