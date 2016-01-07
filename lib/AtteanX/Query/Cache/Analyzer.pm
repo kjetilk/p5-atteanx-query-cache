a31736a3b98eb815d1b264e1495c466606f6717e
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2016-01-07 23:39:21 +0100

    port the predicate count analysis from the trine based code

--- lib/AtteanX/Query/Cache/Analyzer.pm
+++ lib/AtteanX/Query/Cache/Analyzer.pm
@@ -30,6 +30,23 @@ has 'improvement_top' => (is => 'ro', isa => Int, default => '3');
 
 with 'MooX::Log::Any';
 
+=pod
+
+=over
+
+=item C<< store >>
+
+A L<Redis::Fast> object. This has two purposes: First, to store any
+data the analyzer needs to persist to decide when to prefetch. Second,
+it uses Redis' publish-subscribe system to publish the URLs containing
+queries that the prefetcher should fetch.
+
+=cut
+
+has store => (is => 'ro',
+				  isa => InstanceOf['Redis::Fast'],
+				  required => 1
+				 );
 
 sub best_cost_improvement {
 	my $self = shift;
@@ -68,5 +85,43 @@ sub best_cost_improvement {
 	my @worthy = map { $triples{$_} } sort {$costs{$a} <=> $costs{$b}} keys(%triples);
 	return splice(@worthy,0, $self->improvement_top-1);
 }
+
+
+=item C<< count_patterns >>
+
+Loops the triple patterns, checks if any of them have a cached result
+(TODO) and increments the number of times a certain predicate has been
+seen in the store. When that number exceeds the C<count_threshold>, a
+single-element array of L<Attean::TriplePattern>s will be returned.
+
+
+=back
+
+=cut
+
+sub count_patterns {
+	my $self = shift;
+	my $parser = AtteanX::Parser::SPARQL->new();
+	my ($algebra) = $parser->parse_list_from_bytes($self->query, $self->base_uri);
+	my @worthy = ();
+	# TODO: Return undef if we can't process the query
+	foreach my $bgp ($algebra->subpatterns_of_type('Attean::Algebra::BGP')) {
+		foreach my $triple (@{ $bgp->triples }) { # TODO: May need quads
+			my $patternkey = $triple->canonicalize->tuples_string; # This is the key for the triple we process
+			next if ($self->model->is_cached($patternkey));
+			my $key = $triple->predicate->as_string; # This is the key for the predicate we count
+			# Update the storage and return the triple pattern
+			$self->store->incr($key);
+			my $count = $self->store->get($key);
+			$self->log->debug("Count for this key in database is $count");
+			if ($count >= $self->count_threshold) { # TODO: A way to expire counts
+				$self->log->info("Triple $patternkey has predicate with $count ");
+				push(@worthy, $triple);
+			}
+		}
+	}
+	return @worthy;
+}
+
 
 1;
