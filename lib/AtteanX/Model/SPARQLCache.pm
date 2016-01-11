b30999bee122e566da883015e19c99a582fb1b91
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2016-01-11 23:11:02 +0100

    move the BGP cost to their own model

--- lib/AtteanX/Model/SPARQLCache.pm
+++ lib/AtteanX/Model/SPARQLCache.pm
@@ -25,16 +25,9 @@ sub cost_for_plan {
  	my $self	= shift;
  	my $plan	= shift;
  	my $planner	= shift;
-	my $joinfactor = ($plan->isa('Attean::Plan::HashJoin')) ? 9 : 10; # Consistently prefer HashJoins unless treated specially
-	if ($plan->isa('AtteanX::Store::SPARQL::Plan::BGP')) {
-		# BGPs should have a cost proportional to the number of triple patterns,
-		# but be much more costly if they contain a cartesian product.
-		if ($plan->children_are_variable_connected) {
-			return 10 * scalar(@{ $plan->children });
-		} else {
-			return 100 * scalar(@{ $plan->children });
-		}
-# 	} elsif ($plan->does('Attean::API::Plan::Join')) {
+#	my $joinfactor = ($plan->isa('Attean::Plan::HashJoin')) ? 9 : 10; # Consistently prefer HashJoins unless treated specially
+
+# 	if ($plan->does('Attean::API::Plan::Join')) {
 # 		if (${$plan->children}[0]->isa('Attean::Plan::Table') && ${$plan->children}[1]->isa('AtteanX::Store::SPARQL::Plan::BGP')) {
 # 			my $bgpcost	= $planner->cost_for_plan(${$plan->children}[1], $self);
 # 			my $cost	= int($bgpcost * $joinfactor / 20);
@@ -46,7 +39,8 @@ sub cost_for_plan {
 # # 			say "2 Join costs: $bgpcost => $cost\n";
 # 			return $cost;
 # 		}
-	} elsif ($plan->isa('Attean::Plan::Table')) {
+#	} els
+	if ($plan->isa('Attean::Plan::Table')) {
  		return 2;
 	} elsif ($plan->isa('Attean::Plan::Quad')) {
  		return 100000;
