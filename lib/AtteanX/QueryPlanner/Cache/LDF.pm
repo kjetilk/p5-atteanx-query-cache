6683f0eea2ceac11a888005f1a792f598cb0590b
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2016-01-20 01:00:21 +0100

    Create simple queryplanner with LDF

---
+++ lib/AtteanX/QueryPlanner/Cache/LDF.pm
@@ -0,0 +1,17 @@
+use 5.010001;
+use strict;
+use warnings;
+
+
+package AtteanX::QueryPlanner::Cache::LDF;
+use Class::Method::Modifiers;
+
+our $AUTHORITY = 'cpan:KJETILK';
+our $VERSION   = '0.001';
+
+use Moo;
+
+extends 'Attean::QueryPlanner::Cache';
+with 'AtteanX::Query::AccessPlan::LDF';
+
+1;
