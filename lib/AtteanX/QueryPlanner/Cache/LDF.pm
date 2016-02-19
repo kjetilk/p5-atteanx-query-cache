16b6df66544e82ff87b63b4f221a970939fd44bf
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2016-02-19 11:41:57 +0100

    non-working attempt at using before for the allow_join_rotation

--- lib/AtteanX/QueryPlanner/Cache/LDF.pm
+++ lib/AtteanX/QueryPlanner/Cache/LDF.pm
@@ -15,4 +15,17 @@ use AtteanX::Query::AccessPlan::LDF;
 extends 'AtteanX::QueryPlanner::Cache';
 with 'AtteanX::Query::AccessPlan::LDF';
 
+before 'allow_join_rotation' => sub {
+	$_[2] //= 0;
+	my @grandchildren;
+ 	$_[0]->log->trace("Seeking to rotate LDFs in:\n" . $_[1]->as_string);
+	foreach my $p (@{ $_[1]->children }) {
+		$_[2]++ if ($p->isa('AtteanX::Store::LDF::Plan::Triple'));
+	}
+
+	foreach my $p (@grandchildren) {
+		$_[2]++ if ($p->isa('AtteanX::Store::LDF::Plan::Triple'));
+	}
+};
+
 1;
