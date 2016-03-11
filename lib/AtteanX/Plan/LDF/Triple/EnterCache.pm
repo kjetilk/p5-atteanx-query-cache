0e697bb04d2db9c6dfe07eedc749c47f45b758c9
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2016-03-11 23:56:49 +0100

    canonicalize return 1 at the end

---
+++ lib/AtteanX/Plan/LDF/Triple/EnterCache.pm
@@ -0,0 +1,25 @@
+package AtteanX::Plan::LDF::Triple::EnterCache;
+use v5.14;
+use warnings;
+
+our $AUTHORITY = 'cpan:KJETILK';
+our $VERSION = '0.01';
+
+use Moo;
+use Class::Method::Modifiers;
+use Attean;
+use Carp;
+use namespace::clean;
+
+extends 'AtteanX::Plan::LDF::Triple';
+
+around 'impl' => sub {
+	my $orig = shift;
+	my @params = @_;
+	my $self	= shift;
+	my $model	= shift;
+	$model->pubsub->publish('prefetch.triplepattern', $self->canonicalize->tuples_string);
+	return $orig->(@params);
+};
+
+1;
