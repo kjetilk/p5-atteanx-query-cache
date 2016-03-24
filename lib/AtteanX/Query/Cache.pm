465b8faeac32c75bb76984b2628628c5f9927e42
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2016-03-24 02:17:55 +0100

    Create separate plack app

--- lib/AtteanX/Query/Cache.pm
+++ lib/AtteanX/Query/Cache.pm
@@ -10,57 +10,12 @@ use Moo;
 
 extends 'AtteanX::Endpoint';
 
-
-
-package Plack::App::AtteanX::Query::Cache;
-
-our $AUTHORITY = 'cpan:KJETILK';
-our $VERSION   = '0.001_03';
-
-use Attean;
-use Attean::RDF;
-use RDF::Trine;
-use Moo;
-use AtteanX::Endpoint;
-use CHI;
-use Redis;
-use LWP::UserAgent::CHICaching;
-use AtteanX::Model::SPARQLCache::LDF;
-use AtteanX::QueryPlanner::Cache::LDF;
-use Try::Tiny;
-
-extends 'Plack::App::AtteanX::Endpoint';
-with 'MooX::Log::Any';
-
-sub prepare_app {
-	my $self = shift;
-	my $config = $self->{config};
-	my $cache = CHI->new( driver => 'Memory', global => 1 );
-	my $sparqlurl = 'http://dbpedia.org/sparql';
-	my $ldfurl = 'http://fragments.dbpedia.org/2015/en';
-	my $redisserver = 'robin.kjernsmo.net:6379';
-	my $sparqlstore = Attean->get_store('SPARQL')->new(endpoint_url => $sparqlurl);
-	my $ldfstore    = Attean->get_store('LDF')->new(start_url => $ldfurl);
-	my $redissub = Redis->new(server => $redisserver, name => 'subscriber');
-
-	RDF::Trine::default_useragent(LWP::UserAgent::CHICaching->new(cache => $cache));
-
-	my $model	= AtteanX::Model::SPARQLCache::LDF->new( store => $sparqlstore,
-																		  ldf_store => $ldfstore,
-																		  cache => $cache,
-																		  publisher => $redissub);
-	$self->{config} = {};
-
-#	try {
-	$self->{endpoint} = Attean::Endpoint->new(model => $model,
-															planner => AtteanX::QueryPlanner::Cache::LDF->new,
-															conf => $self->{config},
-															graph => iri('http://example.org/graph'));
-	#	  };
-#	if ($@) {
-#		$self->log->error($@);
-#	}
-}
+after 'log_query' => sub {
+	my $self	= shift;
+	my $req		= shift;
+	my $message	= shift;
+	$self->model->publisher->publish('analyze.fullquery', $message);
+};
 
 1;
 
